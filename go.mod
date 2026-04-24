d3a824f6c3a7180ae8c4ec73e1ee3b65be1ad1a4
Andrew Snodgrass <pointlander@gmail.com>
2026-04-23 23:07:04 -0500

    Added qg

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module github.com/pointlander/qg
 go 1.25.0
 
 require (
+	codeberg.org/go-fonts/liberation v0.5.0 // indirect
+	codeberg.org/go-latex/latex v0.2.0 // indirect
+	codeberg.org/go-pdf/fpdf v0.11.1 // indirect
+	git.sr.ht/~sbinet/gg v0.7.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/pointlander/gradient v0.0.0-20260413194610-2e7d5a9f8eaf // indirect
+	golang.org/x/image v0.30.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	gonum.org/v1/plot v0.17.0 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,12 +6,22 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
 	"math"
 	"math/cmplx"
 	"math/rand"
+	"os"
+	"sort"
 	"strings"
 
 	"github.com/pointlander/gradient/tc128"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 const (
@@ -91,7 +101,7 @@ func NewG(rows, cols int) G {
 		}
 		factor := math.Sqrt(2.0 / float64(w.S[0]))
 		for range cap(w.X) {
-			w.X = append(w.X, complex(rng.NormFloat64()*factor, 0))
+			w.X = append(w.X, complex(rng.NormFloat64()*factor, rng.NormFloat64()*factor))
 		}
 		w.States = make([][]complex128, StateTotal)
 		for ii := range w.States {
@@ -99,7 +109,7 @@ func NewG(rows, cols int) G {
 		}
 	}
 	for i := range set.ByName["g"].X {
-		set.ByName["g"].X[i] = 1e-5
+		set.ByName["g"].X[i] = complex(1e-5, 1e-5)
 	}
 	//set.ByName["l"].X[0] = U
 	return G{
@@ -241,6 +251,261 @@ func (g *G) Iterate(inputs Matrix[complex128], iterations int) ([]complex128, []
 	return g.Set.ByName["g"].X, outputs
 }
 
-func main() {
+// SMode s mode
+func SMode(epochs int, iterate func(inputs Matrix[complex128], iterations int) ([]complex128, [][]complex128)) {
+	rng := rand.New(rand.NewSource(1))
+	g := NewMatrix[complex128](Width-1, 33)
+	for range g.Rows {
+		for range g.Cols {
+			g.Data = append(g.Data, complex(rng.Float64(), rng.Float64()))
+		}
+	}
+	getadj := func() Matrix[complex128] {
+		gadj := NewMatrix[complex128](g.Rows, g.Rows)
+		for i := range g.Rows {
+			for j := range g.Rows {
+				sum := 0.0
+				for k := range g.Cols {
+					diff := cmplx.Abs(g.Data[i*g.Cols+k] - g.Data[j*g.Cols+k])
+					sum += diff * diff
+				}
+				distance := math.Sqrt(sum)
+				if distance == 0 {
+					gadj.Data = append(gadj.Data, 0)
+					continue
+				}
+				gadj.Data = append(gadj.Data, complex(1/distance, 0))
+			}
+		}
+		return gadj
+	}
+	gadj := getadj()
+	fmt.Println(gadj.Data)
+	images := &gif.GIF{}
+	var palette = []color.Color{}
+	for i := range 256 {
+		g := byte(i)
+		palette = append(palette, color.RGBA{g, g, g, 0xff})
+	}
+	delay := make([][]chan float64, g.Rows)
+	for i := range delay {
+		delay[i] = make([]chan float64, g.Cols)
+		for ii := range delay[i] {
+			delay[i][ii] = make(chan float64, 2)
+		}
+	}
+	gs := make(plotter.XYs, 0, 8)
+	gavg := make(plotter.XYs, 0, 8)
+	var gshist plotter.Values
+	for epoch := range epochs {
+		fmt.Println(epoch)
+		G, outputs := iterate(gadj, 512)
+		for i := range outputs {
+			type R struct {
+				R float64
+				I int
+			}
+			r := make([]R, g.Rows)
+			for ii := range r {
+				r[ii].R = cmplx.Abs(gadj.Data[i*gadj.Rows+ii])
+				r[ii].I = ii
+			}
+			sort.Slice(r, func(i, j int) bool {
+				return r[i].R < r[j].R
+			})
+			//split := r[len(r)/2]
+			for ii := range outputs[i][:3] {
+				v := outputs[i][ii]
+				/*if v > split.R {
+					select {
+					case vv := <-delay[i][ii]:
+						delay[i][ii] <- v
+						g.Data[i*g.Cols+ii] += vv
+					default:
+						delay[i][ii] <- v
+					}
+				} else {*/
+				g.Data[i*g.Cols+ii] += v * outputs[i][3]
+				//}
+			}
+		}
+		if epoch < 1024 {
+			image := image.NewPaletted(image.Rect(0, 0, 1024, 1024), palette)
+			type Offset struct {
+				X int
+				Y int
+				A int
+				B int
+			}
+			offsets := []Offset{{0, 0, 0, 1}, {512, 0, 0, 2}, {0, 512, 1, 2}}
+			for _, offset := range offsets {
+				minX, maxX, minY, maxY := math.MaxFloat64, -math.MaxFloat64, math.MaxFloat64, -math.MaxFloat64
+				for i := range g.Rows {
+					x, y := cmplx.Abs(g.Data[i*g.Cols+offset.A]), cmplx.Abs(g.Data[i*g.Cols+offset.B])
+					if x < minX {
+						minX = x
+					}
+					if x > maxX {
+						maxX = x
+					}
+					if y < minY {
+						minY = y
+					}
+					if y > maxY {
+						maxY = y
+					}
+				}
+				for i := range g.Rows {
+					xx, yy := cmplx.Abs(g.Data[i*g.Cols+offset.A]), cmplx.Abs(g.Data[i*g.Cols+offset.B])
+					x := 500*(xx-minX)/(maxX-minX) + 6
+					y := 500*(yy-minY)/(maxY-minY) + 6
+					image.Set(offset.X+int(x), offset.Y+int(y), color.RGBA{0xff, 0xff, 0xff, 0xff})
+				}
+			}
+			images.Image = append(images.Image, image)
+			images.Delay = append(images.Delay, 10)
+		}
+		gadj = getadj()
+		avg := complex128(0.0)
+		for _, value := range G {
+			avg += value
+		}
+		avg /= complex(float64(len(G)), 0)
+		stddev := complex128(0.0)
+		for _, value := range G {
+			diff := value - avg
+			stddev = diff * diff
+		}
+		stddev /= complex(float64(len(G)), 0)
+		stddev = cmplx.Sqrt(stddev)
+		fmt.Println("G", avg, stddev)
+		gavg = append(gavg, plotter.XY{X: float64(epoch), Y: float64(cmplx.Abs(avg))})
+		for _, G := range G {
+			gs = append(gs, plotter.XY{X: float64(epoch), Y: float64(cmplx.Abs(G))})
+			gshist = append(gshist, float64(cmplx.Abs(G)))
+		}
+		//gg = G
+	}
 
+	{
+		out, err := os.Create("verse.gif")
+		if err != nil {
+			panic(err)
+		}
+		defer out.Close()
+		err = gif.EncodeAll(out, images)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	{
+		fmt.Println(g.Data)
+
+		p := plot.New()
+
+		p.Title.Text = "G vs time"
+		p.X.Label.Text = "time"
+		p.Y.Label.Text = "G"
+
+		scatter, err := plotter.NewScatter(gs)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(1)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, "G.png")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	{
+		p := plot.New()
+
+		p.Title.Text = "G vs time"
+		p.X.Label.Text = "time"
+		p.Y.Label.Text = "G"
+
+		scatter, err := plotter.NewScatter(gavg)
+		if err != nil {
+			panic(err)
+		}
+		scatter.GlyphStyle.Radius = vg.Length(1)
+		scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(scatter)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, "Gavg.png")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	{
+		/*p := plot.New()
+		p.Title.Text = "G"
+
+		hist, err := plotter.NewHist(gshist, 256)
+		if err != nil {
+			panic(err)
+		}
+		max, index := 0.0, 0
+		for i, bin := range hist.Bins {
+			if bin.Weight > max {
+				max, index = bin.Weight, i
+			}
+		}
+		{
+			min, max := hist.Bins[index].Min, hist.Bins[index].Max
+			fmt.Println("min max", min, max)
+			fmt.Println("min^2 max^2", min*min, max*max)
+			fmt.Println("min/c max/c", min/V, max/V)
+		}
+		p.Add(hist)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, "Ghist.png")
+		if err != nil {
+			panic(err)
+		}
+
+		sort.Slice(hist.Bins, func(i, j int) bool {
+			return hist.Bins[i].Weight < hist.Bins[j].Weight
+		})
+		for i := range hist.Bins {
+			fmt.Println(hist.Bins[i])
+		}*/
+
+		fmt.Println()
+		histogram := make(map[int]int)
+		for _, value := range gshist {
+			exp := int(math.Floor(math.Log10(math.Abs(value))))
+			count := histogram[exp]
+			count++
+			histogram[exp] = count
+		}
+		type Count struct {
+			Count int
+			Exp   int
+		}
+		counts := make([]Count, 0, len(histogram))
+		for key, value := range histogram {
+			counts = append(counts, Count{
+				Count: value,
+				Exp:   key,
+			})
+		}
+		sort.Slice(counts, func(i, j int) bool {
+			return counts[i].Count < counts[j].Count
+		})
+		for _, count := range counts {
+			fmt.Println(count.Exp, ":", count.Count)
+		}
+	}
+}
+
+func main() {
+	g := NewG(33, 33)
+	SMode(1024, g.Iterate)
 }
